f695aeb9ad16640c6f2ade9b97a92ae9f22d9907
hye-on <ain0103@naver.com>
2024-08-03 22:03:16 +0900

    chore: NOT_FOUND status code 값 추가

--- src/main/java/com/wanted/wanted_pre_onboarding_backend/common/StatusCode.java
+++ src/main/java/com/wanted/wanted_pre_onboarding_backend/common/StatusCode.java
@@ -9,6 +9,7 @@ public enum StatusCode {
     SUCCESS(HttpStatus.OK, "요청에 성공하였습니다."),
     SUCCESS_CREATED(HttpStatus.CREATED, "생성되었습니다."),
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "요청 값을 확인해주세요."),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "요청 값을 찾을 수 없습니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 통신 중 오류가 발생하였습니다.."),
     ;
 
