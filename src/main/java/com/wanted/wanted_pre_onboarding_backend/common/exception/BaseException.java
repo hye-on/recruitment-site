8f19ef2c8e55f3568e03fa9ff8b68a6bef1f0e98
hye-on <ain0103@naver.com>
2024-08-03 21:55:43 +0900

    feat: common 컴포넌트 생성

---
+++ src/main/java/com/wanted/wanted_pre_onboarding_backend/common/exception/BaseException.java
@@ -0,0 +1,24 @@
+package com.wanted.wanted_pre_onboarding_backend.common.exception;
+
+import com.wanted.wanted_pre_onboarding_backend.common.StatusCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class BaseException extends RuntimeException {
+
+    private StatusCode statusCode;
+
+    public BaseException(String message, Throwable cause, StatusCode statusCode) {
+        super(message, cause);
+        this.statusCode = statusCode;
+    }
+
+    public BaseException(Throwable cause, StatusCode statusCode) {
+        super(cause);
+        this.statusCode = statusCode;
+    }
+}
--- src/main/java/com/wanted/wanted_pre_onboarding_backend/common/exception/EmptyException.java
+++
@@ -1,4 +0,0 @@
-package com.wanted.wanted_pre_onboarding_backend.common.exception;
-
-public class EmptyException {
-}
