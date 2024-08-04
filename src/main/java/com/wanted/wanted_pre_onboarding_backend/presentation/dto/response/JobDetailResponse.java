86e8f0e1014ff38bdbc4c7e71c5780e47f7fa602
hye-on <ain0103@naver.com>
2024-08-05 00:24:17 +0900

    feat: Entity to Response 함수 추가 (정적 팩토리 메서드 패턴)

--- src/main/java/com/wanted/wanted_pre_onboarding_backend/presentation/dto/response/JobDetailResponse.java
+++ src/main/java/com/wanted/wanted_pre_onboarding_backend/presentation/dto/response/JobDetailResponse.java
@@ -2,14 +2,19 @@ package com.wanted.wanted_pre_onboarding_backend.presentation.dto.response;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
+
+import com.wanted.wanted_pre_onboarding_backend.domain.entity.Job;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Schema(description = "채용공고 상세 정보")
 @Builder
 @Getter
+@AllArgsConstructor
 public class JobDetailResponse {
 
 	@Schema(description = "회사명", example = "원티드")
@@ -39,6 +44,7 @@ public class JobDetailResponse {
 	@Schema(description = "회사의 다른 채용공고")
 	@Builder
 	@Getter
+	@AllArgsConstructor
 	public static class OtherJob {
 
 		@Schema(description = "채용공고 id", example = "2c0227b4-5a7b-4094-87a9-f9a36627bd84")
@@ -53,5 +59,30 @@ public class JobDetailResponse {
 		@Schema(description = "기술 리스트", example = "[\"NestJS\", \"TypeScript\", \"Prisma\"]")
 		private List<String> skillList;
 
+		public static OtherJob from(Job job) {
+			return OtherJob.builder()
+				.jobId(job.getId())
+				.position(job.getPosition())
+				.recruitmentBonus(job.getRecruitmentBonus())
+				.skillList(job.getJobSkills().stream().map(o -> o.getSkill().toString()).toList())
+				.build();
+		}
+	}
+
+	public static JobDetailResponse from(Job currentJob, List<Job> jobList) {
+		List<OtherJob> otherJobList = jobList.stream()
+			.map(OtherJob::from)
+			.collect(Collectors.toList());
+
+		return JobDetailResponse.builder()
+			.companyName(currentJob.getCompany().getName())
+			.countryName(currentJob.getCompany().getCountryName())
+			.locationName(currentJob.getCompany().getLocationName())
+			.position(currentJob.getPosition())
+			.recruitmentBonus(currentJob.getRecruitmentBonus())
+			.description(currentJob.getDescription())
+			.skillList(currentJob.getJobSkills().stream().map(o -> o.getSkill().toString()).toList())
+			.otherJobList(otherJobList)
+			.build();
 	}
 }
